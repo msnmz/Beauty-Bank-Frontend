a53f342b305d670c57a76110cfd6c65b6a344298
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2021-02-04 19:33:49 +0100

    Cretaed Request class

---
+++ src/services/Request.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export default class Request {
+
+    static async getData(path) {
+        const token = 'token';
+        const response = await axios.get(path, {
+            headers: { token, },
+        });
+
+        return response?.data;
+    }
+
+    static async postData(path, data) {
+        const token = localStorage.getItem('token');
+        const response = await axios.post(path, data, {
+            headers: {
+                token
+            }
+        });
+        return response?.data;
+    }
+}
+
