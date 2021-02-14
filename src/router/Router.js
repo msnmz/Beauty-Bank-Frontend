5f0d274b20feb6e450d340e35c8b046d22ac9a73
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2021-02-14 21:57:46 +0100

    Added dashboard routes and private route, deleted /signup route

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Signup, Signin, SignupDetail, SignupMenu } from '../pages/Index';
+import {
+    Signup,
+    Signin,
+    SignupDetail,
+    SignupMenu,
+    DashboardAdmin,
+    DashboardClient,
+    DashboardConnector,
+    DashboardProfessional,
+    DashboardSponsor,
+} from '../pages/Index';
 import { Footer } from '../components/Index';
+import { AppContext } from '../context/AppContext';
 
 const AppRouter = () => {
 
+    const { user } = useContext(AppContext);
+    console.log(user);
+    console.log(user?.role);
     return (
         <Router>
             <Switch>
                 <Route exact path='/register' component={SignupMenu} />
-                <Route exact path='/signup' component={Signup} />
-                <Route exact path='/login' component={Signin} />
                 <Route exact path='/register/:id' component={SignupDetail} />
+                <Route exact path='/login' component={Signin} />
+                <Route exact path='/admin' component={DashboardAdmin} />
+                <Route exact path='/client' component={user?.role === 'Client' ? DashboardClient : Signin} />
+                <Route exact path='/connector' component={user?.role === 'Connector' ? DashboardConnector : Signin} />
+                <Route exact path='/professional' component={user?.role === 'Professional' ? DashboardProfessional : Signin} />
+                <Route exact path='/sponsor' component={user?.role === 'Professional' ? DashboardSponsor : Signin} />
             </Switch>
             <Footer />
         </Router>
