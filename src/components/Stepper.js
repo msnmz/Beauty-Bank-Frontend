e6eb604e11f0ac838a3a884144b36e525d65978e
Mr. Tarik OZTURK <mr.tarik.ozturk@gmail.com>
2021-03-18 14:37:02 +0100

    Created Stepper component.

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,93 +1,80 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import MuiStepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        width: '100%',
+        width: "100%"
     },
     backButton: {
-        marginRight: theme.spacing(1),
+        marginRight: theme.spacing(1)
     },
     instructions: {
         marginTop: theme.spacing(1),
-        marginBottom: theme.spacing(1),
-    },
+        marginBottom: theme.spacing(1)
+    }
 }));
 
-function getSteps() {
-    return ['Select master blaster campaign settings', 'Create an ad group', 'Create an ad'];
-}
+const getSteps = () => {
+    return [
+        "Created Ticket",
+        "Approve Terms",
+        "Set Ticket Date",
+        "Waiting for the appointment",
+        "Feedback"
+    ];
+};
 
-function getStepContent(stepIndex) {
+const getStepContent = (stepIndex) => {
     switch (stepIndex) {
         case 0:
-            return 'Select campaign settings...';
+            return "Our connector will call you...";
         case 1:
-            return 'What is an ad group anyways?';
+            return "Please, check your e-mail box and approve the terms.";
         case 2:
-            return 'This is the bit I really care about!';
+            return "Please, set your ticket date.";
+        case 3:
+            return "Please, don't forget your appointment!";
+        case 4:
+            return "Please, write feedback for better service...";
         default:
-            return 'Unknown stepIndex';
+            return "There is a problem, please call us.";
     }
-}
+};
 
-const Steps = () => {
+const Stepper = ({ activeStep = 0 }) => {
+    // constants
     const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState(0);
     const steps = getSteps();
-
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleReset = () => {
-        setActiveStep(0);
-    };
 
     return (
         <div className={classes.root}>
-            <Stepper activeStep={activeStep} alternativeLabel >
-                {steps.map((label) => (
-                    <Step key={label}>
-                        <StepLabel>{label}</StepLabel>
+            <MuiStepper activeStep={activeStep} alternativeLabel>
+                {steps.map((item) => (
+                    <Step key={item}>
+                        <StepLabel>{item}</StepLabel>
                     </Step>
                 ))}
-            </Stepper>
+            </MuiStepper>
             <div>
                 {activeStep === steps.length ? (
                     <div>
-                        <Typography className={classes.instructions}>All steps completed</Typography>
-                        <Button onClick={handleReset}>Reset</Button>
+                        <Typography className={classes.instructions}>
+                            All steps completed
+            </Typography>
                     </div>
                 ) : (
-                        <div>
-                            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-                            <div>
-                                <Button
-                                    disabled={activeStep === 0}
-                                    onClick={handleBack}
-                                    className={classes.backButton}
-                                >
-                                    Back
-                                </Button>
-                                <Button variant="contained" color="primary" onClick={handleNext}>
-                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                </Button>
-                            </div>
-                        </div>
-                    )}
+                    <div>
+                        <Typography className={classes.instructions}>
+                            {getStepContent(activeStep)}
+                        </Typography>
+                    </div>
+                )}
             </div>
         </div>
     );
-}
+};
 
-export { Steps };
+export default Stepper;
